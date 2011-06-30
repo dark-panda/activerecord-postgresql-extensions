640abf35b95df6da8918de2c11027d6a81f27c65
J Smith <jay@zoocasa.com>
2011-06-30 16:28:31 -0400

    Add a test to make sure the geometry type constraint works.

--- test/geometry_test.rb
+++ test/geometry_test.rb
@@ -58,4 +58,23 @@ class GeometryTests < Test::Unit::TestCase
   %{CREATE INDEX "foo_the_geom_gist_index" ON "foo" USING "gist"("the_geom")}
     ], statements)
   end
+
+  def test_create_geometry_with_null_and_type
+    Mig.create_table(:foo) do |t|
+      t.geometry :the_geom, :srid => 4326, :geometry_type => :polygon
+    end
+
+    assert_equal([
+      %{CREATE TABLE "foo" (
+  "id" serial primary key,
+  "the_geom" geometry,
+  CONSTRAINT "enforce_srid_the_geom" CHECK (srid("the_geom") = (4326)),
+  CONSTRAINT "enforce_dims_the_geom" CHECK (ndims("the_geom") = 2),
+  CONSTRAINT "enforce_geotype_the_geom" CHECK (geometrytype("the_geom") = 'POLYGON'::text OR "the_geom" IS NULL)
+)},
+  %{DELETE FROM "geometry_columns" WHERE f_table_catalog = '' AND f_table_schema = 'public' AND f_table_name = 'foo' AND f_geometry_column = 'the_geom'},
+  %{INSERT INTO "geometry_columns" VALUES ('', 'public', 'foo', 'the_geom', 2, 4326, 'POLYGON')},
+  %{CREATE INDEX "foo_the_geom_gist_index" ON "foo" USING "gist"("the_geom")}
+    ], statements)
+  end
 end
