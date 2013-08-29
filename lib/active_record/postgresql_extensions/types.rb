afdbfdc7387adf17898dcca883a6526b6988e00f
J Smith <jay@zoocasa.com>
2013-08-29 15:42:51 -0400

    Add some initial support for creating TYPEs starting with ENUMs.

--- lib/active_record/postgresql_extensions/types.rb
+++ lib/active_record/postgresql_extensions/types.rb
@@ -2,6 +2,9 @@
 require 'active_record/connection_adapters/postgresql_adapter'
 
 module ActiveRecord
+  class InvalidAddEnumValueOptions < ActiveRecordError #:nodoc:
+  end
+
   module ConnectionAdapters
     class PostgreSQLAdapter
       # Returns an Array of available languages.
@@ -28,6 +31,92 @@ module ActiveRecord
       def type_exists?(name)
         types.include?(name.to_s)
       end
+
+      # Creates an ENUM TYPE. An ENUM can contain zero or more values. ENUMs
+      # can be dropped with #drop_type.
+      def create_enum(name, *values)
+        execute PostgreSQLEnumDefinition.new(self, name, *values).to_s
+      end
+
+      # Adds a new value to an ENUM.
+      #
+      # ==== Options
+      #
+      # * <tt>:before</tt> - add the new value before this value.
+      # * <tt>:after</tt> - add the new value after this value.
+      # * <tt>:if_not_exists</tt> - adds the value if it doesn't already
+      #   exist. Available in PostgreSQL 9.3+.
+      def add_enum_value(enum, value, options = {})
+        assert_valid_add_enum_value_options(options)
+
+        sql = "ALTER TYPE #{quote_generic(enum)} ADD VALUE"
+
+        if options[:if_not_exists]
+          sql << " IF NOT EXISTS"
+        end
+
+        sql << " #{quote(value)}"
+
+        if options[:before]
+          sql << " BEFORE #{quote(options[:before])}"
+        elsif options[:after]
+          sql << " AFTER #{quote(options[:after])}"
+        end
+
+        execute("#{sql};")
+      end
+
+      # Drop TYPEs.
+      #
+      # ==== Options
+      #
+      # * <tt>:if_exists</tt> - adds IF EXISTS.
+      # * <tt>:cascade</tt> - adds CASCADE.
+      def drop_type(*args)
+        options = args.extract_options!
+        args.flatten!
+
+        sql = 'DROP TYPE '
+        sql << 'IF EXISTS ' if options[:if_exists]
+        sql << Array(args).collect { |i| quote_generic(i) }.join(', ')
+        sql << ' CASCADE' if options[:cascade]
+        execute("#{sql};")
+      end
+
+      # Returns an Array of possible
+      def enum_values(name)
+        query(%{SELECT unnest(enum_range(NULL::#{quote_generic(name)}))}, 'Enum values').map(&:first)
+      end
+
+      private
+        def assert_valid_add_enum_value_options(options)
+          if options[:before] && options[:after]
+            raise InvalidAddEnumValueOptions.new("Can't use both :before and :after options together")
+          end
+
+          if options[:if_not_exists] && ActiveRecord::PostgreSQLExtensions.SERVER_VERSION < '9.3'
+            raise InvalidAddEnumValueOptions.new("The :if_not_exists option is only available in PostgreSQL 9.3+.")
+          end
+        end
     end
+  end
+
+  # Creates a PostgreSQL enum type definition. This class isn't really meant
+  # to be used directly. Instead, see PostgreSQLAdapter#create_enum for
+  # usage.
+  class PostgreSQLEnumDefinition
+    attr_accessor :base, :name, :values
+
+    def initialize(base, name, *values)
+      @base, @name, @values = base, name, values.flatten
+    end
+
+    def to_sql #:nodoc:
+      sql = "CREATE TYPE #{base.quote_generic(name)} AS ENUM ("
+      sql << values.collect { |t| base.quote(t.to_s) }.join(', ')
+      sql << ")"
+      "#{sql};"
+    end
+    alias :to_s :to_sql
   end
 end
--- test/types_tests.rb
+++ test/types_tests.rb
@@ -20,4 +20,78 @@ class TypesTests < MiniTest::Unit::TestCase
       assert(ARBC.type_exists?('oid'))
     end
   end
+
+  class EnumTests < MiniTest::Unit::TestCase
+    include PostgreSQLExtensionsTestHelper
+
+    def setup
+      ARBC.real_execute do
+        Mig.drop_type(:foo_enum, :if_exists => true)
+      end
+
+      super
+    end
+
+    def test_create_enum
+      Mig.create_enum(:foo_enum)
+      Mig.create_enum(:foo_enum, %w{ one two three })
+      Mig.create_enum(:foo_enum, :one, :two, :three)
+
+      assert_equal([
+        %{CREATE TYPE "foo_enum" AS ENUM ();},
+        %{CREATE TYPE "foo_enum" AS ENUM ('one', 'two', 'three');},
+        %{CREATE TYPE "foo_enum" AS ENUM ('one', 'two', 'three');}
+      ], statements)
+    end
+
+    def test_add_enum_value
+      Mig.create_enum(:foo_enum)
+      Mig.add_enum_value(:foo_enum, :foo)
+      Mig.add_enum_value(:foo_enum, :bar, :before => :foo)
+      Mig.add_enum_value(:foo_enum, :blort, :after => :foo)
+
+      assert_equal([
+        %{CREATE TYPE "foo_enum" AS ENUM ();},
+        %{ALTER TYPE "foo_enum" ADD VALUE 'foo';},
+        %{ALTER TYPE "foo_enum" ADD VALUE 'bar' BEFORE 'foo';},
+        %{ALTER TYPE "foo_enum" ADD VALUE 'blort' AFTER 'foo';}
+      ], statements)
+    end
+
+    def test_add_enum_value_if_not_exists
+      skip unless ActiveRecord::PostgreSQLExtensions.SERVER_VERSION >= '9.3'
+
+      Mig.add_enum_value(:foo_enum, :baz, :if_not_exists => true)
+
+      assert_equal([
+        %{ALTER TYPE "foo_enum" ADD VALUE IF NOT EXISTS 'baz';}
+      ], statements)
+    end
+
+    def test_drop_type
+      Mig.drop_type(:foo_enum)
+      Mig.drop_type(:foo_enum, :if_exists => true)
+      Mig.drop_type(:foo_enum, :cascade => true)
+      Mig.drop_type(:foo_enum, :bar_enum)
+
+      assert_equal([
+        %{DROP TYPE "foo_enum";},
+        %{DROP TYPE IF EXISTS "foo_enum";},
+        %{DROP TYPE "foo_enum" CASCADE;},
+        %{DROP TYPE "foo_enum", "bar_enum";}
+      ], statements)
+    end
+
+    def test_enum_values
+      ARBC.real_execute do
+        ARBC.transaction do
+          Mig.create_enum(:foo_enum, :one, :two, :three)
+
+          assert_equal(%w{ one two three }, ARBC.enum_values(:foo_enum))
+
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+  end
 end
