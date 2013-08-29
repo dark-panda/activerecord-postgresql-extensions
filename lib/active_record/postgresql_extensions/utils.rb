3760faf16f4863aa2a2cf7fd0331b6629da0c0bc
J Smith <jay@zoocasa.com>
2013-08-29 13:26:13 -0400

    Allow Utils to be included and include it into itself.

--- lib/active_record/postgresql_extensions/utils.rb
+++ lib/active_record/postgresql_extensions/utils.rb
@@ -2,21 +2,23 @@
 module ActiveRecord
   module PostgreSQLExtensions
     module Utils
-      class << self
-        def hash_or_array_of_hashes(arg)
-          case arg
-            when Hash
-              [ arg ]
-            when Array
-              if arg.detect { |e| !e.is_a?(Hash) }
-                raise ArgumentError.new("Expected an Array of Hashes")
-              else
-                arg
-              end
+      def hash_or_array_of_hashes(arg)
+        case arg
+          when Hash
+            [ arg ]
+          when Array
+            if arg.detect { |e| !e.is_a?(Hash) }
+              raise ArgumentError.new("Expected an Array of Hashes")
             else
-              raise ArgumentError.new("Expected either a Hash or an Array of Hashes")
-          end
+              arg
+            end
+          else
+            raise ArgumentError.new("Expected either a Hash or an Array of Hashes")
         end
+      end
+
+      class << self
+        include Utils
       end
     end
   end
